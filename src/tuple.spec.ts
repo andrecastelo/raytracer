72e3c4dd1c589f21db06c4dca8477aee029a5a1e
Andre Castelo <andrecastelo@gmail.com>
2020-10-10 13:11:54 -0300

    add multiply, divide, magnitude, normalize, dot and cross functions

--- src/tuple.spec.ts
+++ src/tuple.spec.ts
@@ -7,18 +7,24 @@ import {
   equalTuples,
   negateVector,
   negate,
-} from "./tuple";
+  multiply,
+  divide,
+  magnitude,
+  normalize,
+  dot,
+  cross,
+} from './tuple';
 
-describe("Tuples are arrays with 4 numbers", () => {
-  it("point() creates a point with w = 1", () => {
+describe('Tuples are arrays with 4 numbers', () => {
+  it('point() creates a point with w = 1', () => {
     expect(point(4, -4, 3)).toEqual([4, -4, 3, 1.0]);
   });
 
-  it("vector() creates a vector with w = 0", () => {
+  it('vector() creates a vector with w = 0', () => {
     expect(vector(4, -4, 3)).toEqual([4, -4, 3, 0.0]);
   });
 
-  it("equalTuples() returns if tuples are equal", () => {
+  it('equalTuples() returns if tuples are equal', () => {
     const a: Tuple = [1.0, 2.0, 3.0, 1.0];
     const b: Tuple = [1.000005, 1.999995, 2.9999998, 1.0];
     const c: Tuple = [1.0, 2.0, 3.02, 1.0];
@@ -27,7 +33,7 @@ describe("Tuples are arrays with 4 numbers", () => {
     expect(equalTuples(a, c)).toBeFalsy();
   });
 
-  it("addTuples() add tuples properly", () => {
+  it('addTuples() add tuples properly', () => {
     const a: Tuple = point(3, -2, 5);
     const b: Tuple = vector(-2, 3, 1);
     const final: Tuple = point(1, 1, 6);
@@ -35,14 +41,14 @@ describe("Tuples are arrays with 4 numbers", () => {
     expect(equalTuples(addTuples(a, b), final)).toBeTruthy();
   });
 
-  it("addTuples() cannot add two points", () => {
+  it('addTuples() cannot add two points', () => {
     const a: Tuple = point(1, 2, 3);
     const b: Tuple = point(3, 2, 1);
 
     expect(() => addTuples(a, b)).toThrow();
   });
 
-  it("subTuples() can subtract two points", () => {
+  it('subTuples() can subtract two points', () => {
     const a = point(3, 2, 1);
     const b = point(5, 6, 7);
     const expected = vector(-2, -4, -6);
@@ -50,28 +56,78 @@ describe("Tuples are arrays with 4 numbers", () => {
     expect(equalTuples(subTuples(a, b), expected)).toBeTruthy();
   });
 
-  it("subTuples() can subtract a vector from a point", () => {
+  it('subTuples() can subtract a vector from a point', () => {
     const p = point(3, 2, 1);
     const v = vector(5, 6, 7);
 
     expect(equalTuples(subTuples(p, v), point(-2, -4, -6))).toBeTruthy();
   });
 
-  it("subTuples() can subtract two vectors", () => {
+  it('subTuples() can subtract two vectors', () => {
     const v1 = vector(3, 2, 1);
     const v2 = vector(5, 6, 7);
 
     expect(equalTuples(subTuples(v1, v2), vector(-2, -4, -6))).toBeTruthy();
   });
 
-  it("Subtracting a vector from the zero vector", () => {
+  it('Subtracting a vector from the zero vector', () => {
     const v = vector(1, -2, 3);
 
     expect(equalTuples(negateVector(v), vector(-1, 2, -3))).toBeTruthy();
   });
 
-  it("Negating a tuple", () => {
+  it('Negating a tuple', () => {
     const tuple: Tuple = [1, -2, 3, -4];
     expect(equalTuples(negate(tuple), <Tuple>[-1, 2, -3, 4])).toBeTruthy();
+  });
+
+  it('Scalar multiplication and division', () => {
+    const a: Tuple = [1, -2, 3, -4];
+
+    expect(
+      equalTuples(multiply(a, 3.5), <Tuple>[3.5, -7, 10.5, -14])
+    ).toBeTruthy();
+
+    expect(
+      equalTuples(multiply(a, 0.5), <Tuple>[0.5, -1, 1.5, -2])
+    ).toBeTruthy();
+
+    expect(equalTuples(divide(a, 2), <Tuple>[0.5, -1, 1.5, -2])).toBeTruthy();
+  });
+
+  it('Computing the magnitudes', () => {
+    expect(magnitude(vector(1, 0, 0))).toEqual(1);
+    expect(magnitude(vector(0, 1, 0))).toEqual(1);
+    expect(magnitude(vector(0, 0, 1))).toEqual(1);
+
+    expect(magnitude(vector(1, 2, 3))).toEqual(Math.sqrt(14));
+    expect(magnitude(vector(-1, -2, -3))).toEqual(Math.sqrt(14));
+  });
+
+  it('Normalize', () => {
+    expect(normalize(vector(4, 0, 0))).toEqual(vector(1, 0, 0));
+
+    const v = vector(1, 2, 3);
+    expect(
+      equalTuples(normalize(v), vector(0.26726, 0.53452, 0.80178))
+    ).toBeTruthy();
+  });
+
+  it('Dot product', () => {
+    const a = vector(1, 2, 3);
+    const b = vector(2, 3, 4);
+
+    expect(dot(a, b)).toEqual(20);
+
+    expect(dot(vector(1, 0, 0), vector(1, 0, 0))).toEqual(1);
+    expect(dot(vector(1, 0, 0), vector(-1, 0, 0))).toEqual(-1);
+  });
+
+  it('Cross product', () => {
+    const a = vector(1, 2, 3);
+    const b = vector(2, 3, 4);
+
+    expect(cross(a, b)).toEqual(vector(-1, 2, -1));
+    expect(cross(b, a)).toEqual(vector(1, -2, 1));
   });
 });
--- src/tuple.ts
+++ src/tuple.ts
@@ -1,6 +1,11 @@
-import { assert } from "console";
-import { equal } from "./utils";
+import { equal } from './utils';
 
+/**
+ * This will be our basic work horse. I decided to use an array as opposed
+ * to an object because we could use functions instead of methods.
+ *
+ * Plus we get to use map(), every(), and reduce(), which is pretty neat.
+ */
 export type Tuple = [number, number, number, number];
 
 export function point(x: number, y: number, z: number): Tuple {
@@ -19,7 +24,7 @@ export function addTuples(a: Tuple, b: Tuple): Tuple {
   const w = a[3] + b[3];
 
   if (w > 1) {
-    throw "Cannot add two points";
+    throw 'Cannot add two points';
   }
 
   return [a[0] + b[0], a[1] + b[1], a[2] + b[2], a[3] + b[3]];
@@ -37,4 +42,66 @@ export function negateVector(a: Tuple): Tuple {
 
 export function negate(a: Tuple): Tuple {
   return <Tuple>a.map((x) => -x);
+}
+
+export function multiply(a: Tuple, scalar: number): Tuple {
+  return <Tuple>a.map((value) => value * scalar);
+}
+
+export function divide(a: Tuple, scalar: number): Tuple {
+  return <Tuple>a.map((value) => value / scalar);
+}
+
+/**
+ * The vector is a value that encodes both direction and distance. The
+ * distance is the magnitude (or length).
+ *
+ * @param a vector
+ */
+export function magnitude(a: Tuple): number {
+  return Math.sqrt(a.reduce((total, value) => total + Math.pow(value, 2), 0));
+}
+
+/**
+ * Normalization is the process of taking an arbitrary vector and converting
+ * it into a unitvector. It will keep our calculations anchored relative
+ * to a common scale (the unit vector).
+ *
+ * @param a vector
+ */
+export function normalize(a: Tuple): Tuple {
+  const m = magnitude(a);
+
+  return <Tuple>a.map((value) => value / m);
+}
+
+/**
+ * The dot product can feel pretty abstract, but there's one quick way to
+ * internalize it: the smaller the dot product, the larger the angle
+ * between the vectors.
+ *
+ * @param a vector
+ * @param b vector
+ */
+export function dot(a: Tuple, b: Tuple): number {
+  return a.reduce((total, _, index) => total + a[index] * b[index], 0);
+}
+
+/**
+ * The cross product is a vector operation, but instead of the dot product, we
+ * this function returns another vector, perpendicular to both of the original
+ * vecrtors. It will not work with regular tuples.
+ *
+ * @param v1 vector
+ * @param v2 vector
+ */
+export function cross(v1: Tuple, v2: Tuple): Tuple {
+  const a = { x: v1[0], y: v1[1], z: v1[2] };
+  const b = { x: v2[0], y: v2[1], z: v2[2] };
+
+  return vector(
+    a.y * b.z - a.z * b.y,
+    a.z * b.x - a.x * b.z,
+    a.x * b.y - a.y * b.x
+  );
 }
